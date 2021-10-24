795e0da745736efd1de7db69c4f6cd756830e97c
Martynovforgston <79844338+Martynovforgston@users.noreply.github.com>
2021-10-24 13:33:52 +0500

    Add index.php

---
+++ index.php
@@ -0,0 +1,52 @@
+<p> Дата и время:
+<p>
+
+<?php
+
+echo '<title>Мартынов В.Е..</title>';
+
+$d = date("d.m.Y H:i");
+echo ($d);
+
+echo '<h2>Практическая работа №1</h2>';
+
+?>
+
+<ul>
+    <li> <a href="/lab-1-1.php"> Упражнение 1-2 </a> </li>
+    <li> <a href="/lab-1-2.php"> Упражнение 1-3 </a> </li>
+    <li> <a href="/lab-1-3.php"> Упражнение 1-4 </a> </li>
+</ul>
+
+<?php
+echo '<h2>Самостоятельная работа №1</h2>';
+?>
+
+<ul>
+    <li> <a href="/lab-1-5.php"> Задача 1-1 </a> </li>
+</ul>
+
+<?php
+echo '<h2>Практическая работа №2</h2>';
+?>
+
+<ul>
+    <li> <a href="/lab-2-1.php"> Упражнение 1-6 </a> </li>
+    <li> <a href="/lab-2-2.php"> Упражнение 1-7 </a> </li>
+    <li> <a href="/lab-2-3.php"> Упражнение 1-8 </a> </li>
+    <li> <a href="/lab-2-4.php"> Упражнение 1-9 </a> </li>
+    <li> <a href="/lab-2-5.php"> Упражнение 1-10 </a> </li>
+    <li> <a href="/lab-2-6.php"> Упражнение 1-11 </a> </li>
+    <li> <a href="/lab-2-7.php"> Упражнение 1-12 </a> </li>
+    <li> <a href="/lab-2-8.php"> Упражнение 1-13 </a> </li>
+    <li> <a href="/lab-2-9.php"> Упражнение 1-14 </a> </li>
+</ul>
+
+<?php
+echo '<h2>Самостоятельная работа №2</h2>';
+?>
+
+<ul>
+    <li> <a href="/lab-2-10.php"> Задача 1-2 </a> </li>
+    <li> <a href="/lab-2-11.php"> Задача 1-3 </a> </li>
+</ul>
