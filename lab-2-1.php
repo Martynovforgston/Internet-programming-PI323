419de6ca2c1cd2c969ee94c9f28de487d3edd3b4
Martynovforgston <79844338+Martynovforgston@users.noreply.github.com>
2021-10-24 14:39:48 +0500

    Add lab-2-1.php

---
+++ lab-2-1.php
@@ -0,0 +1,13 @@
+<?php
+
+echo '<title>Мартынов В.Е.</title>';
+
+$a = 1; 
+$b = false;
+
+if ($a) { echo '$a='."$a - истина<br>"; }
+else { echo '$a='."$a - ложь<br>"; }
+
+if ($b) { echo '$b='."$b - истина<br>"; }
+else { echo '$b='."$b - ложь<br>"; }
+?>
